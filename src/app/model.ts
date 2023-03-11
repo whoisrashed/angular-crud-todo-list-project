36ea4bab7f28389d71ae1572d6e036e62ff97a1f
whoisrashed <learnwithrash3d@gmail.com>
2023-03-11 23:06:37 +0600

    todo list

---
+++ src/app/model.ts
@@ -0,0 +1,4 @@
+export interface Todo {
+    Title:string;
+    Id:any;
+}
---
+++ src/app/todo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+
+  addTodo(){
+
+  }
+
+
+
+  getTodoList(){
+
+  }
+
+  updateTodo(){
+    
+  }
+}

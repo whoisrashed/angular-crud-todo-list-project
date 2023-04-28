4c650b35ebd56c1453c524afa040c9329654fac3
whoisrashed <learnwithrash3d@gmail.com>
2023-04-28 21:15:01 +0500

    base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Todo } from './model';
 
 
 
@@ -10,57 +9,5 @@ import { Todo } from './model';
 })
 export class AppComponent {
 
-  todoList :Todo []=[];
-  todo:Todo = this.initTodo;
 
-
-
-  get initTodo ():Todo{
-    return {
-      Title:'',
-      Id:null
-    }
-  }
-
-  addTodo():void {
-    console.log(this.todo)
-
-
-    if(this.todo.Id){
-
-    this.todoList = this.todoList.map(o=> {
-
-      if(o.Id == this.todo.Id){
-        o.Title = this.todo.Title;
-      }
-      return o;
-    })
-
-
-      // update logic
-    } 
-    else {
-      // create logic
-      this.todo.Id = Date.now();
-      this.todoList.push({...this.todo});
-    }
-
-  
-
-
-    console.log(this.todoList)
-
-    this.todo =this.initTodo;
-  }
-
-  editTodo(todo:Todo) :void{
-
-    this.todo = {...todo};
-  }
-
-  delteTodo(id:number):void {
-    this.todoList = this.todoList.filter(o=>o.Id!=id);
-
-  }
- 
 }
--- src/app/model.ts
+++
@@ -1,4 +0,0 @@
-export interface Todo {
-    Title:string;
-    Id:any;
-}
